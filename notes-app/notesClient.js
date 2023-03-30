a4dc62db91bfc430a0cc38cccc9d0bc20c9f9ed9
samuelbadru <samuelbadru@outlook.com>
2023-03-30 11:38:44 +0100

    Test driven createNote method

--- notes-app/notesClient.js
+++ notes-app/notesClient.js
@@ -4,6 +4,18 @@ class NotesClient {
       .then(response => response.json())
       .then(data => callback(data));
   }
+
+  createNote(newNote, callback) {
+    fetch('http://localhost:3000/notes', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newNote)
+    })
+    .then(response => response.json())
+    .then(data => callback(data));
+  }
 }
 
 module.exports = NotesClient;
--- notes-app/notesClient.test.js
+++ notes-app/notesClient.test.js
@@ -5,13 +5,29 @@ describe('NotesClient', () => {
   it('loads notes', (done) => {
     const client = new NotesClient();
 
-    fetch.mockResponseOnce(JSON.stringify({
-      note1: "This note is coming from the server"
-    }));
+    fetch.mockResponseOnce(JSON.stringify([
+      "This note is coming from the server"
+    ]));
 
     client.loadNotes((data) => {
-      expect(data.note1).toBe("This note is coming from the server")
+      expect(data).toEqual(["This note is coming from the server"])
       done();
     });
+  });
+
+  it('creates a new note', (done) => {
+    const client = new NotesClient();
+
+    const newNote = "Remember to reflect on my progress this week!";
+
+    fetch.mockResponseOnce(JSON.stringify([
+      "This note is coming from the server",
+      "Remember to reflect on my progress this week!"
+    ]));
+
+    client.createNote(newNote, (data) => {
+      expect(data).toEqual(["This note is coming from the server", "Remember to reflect on my progress this week!"])
+      done();
+    })
   });
 });
